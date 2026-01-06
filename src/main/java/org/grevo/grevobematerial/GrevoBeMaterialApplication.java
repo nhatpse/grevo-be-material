bdd55b0ce66a3fcc7ceeb9bd2ff438ecb6d7fca0
nhatpse <pnhatse@gmail.com>
2026-01-06 09:19:52 +0700

    g001-first_commit

---
+++ src/main/java/org/grevo/grevobematerial/GrevoBeMaterialApplication.java
@@ -0,0 +1,13 @@
+package org.grevo.grevobematerial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrevoBeMaterialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrevoBeMaterialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/grevo/grevobematerial/GrevoBeMaterialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grevo.grevobematerial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrevoBeMaterialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
