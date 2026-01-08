b1adea4f66491d972c2f17948849eeb1fc93bc94
nhatpse <pnhatse@gmail.com>
2026-01-08 18:23:20 +0700

    [G-004] Update: Setup Swagger

--- src/main/java/org/grevo/grevobematerial/config/SecurityConfig.java
+++ src/main/java/org/grevo/grevobematerial/config/SecurityConfig.java
@@ -49,7 +49,12 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                                ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
